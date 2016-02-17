f0c202ca38d6b23bf94dd856c893fa66b52c09eb
Coordes <lennard.coordes@btc-ag.com>
2016-02-17 10:26:02 +0100

    Initial commit

---
+++ src/main/java/com/btc/lecoorde/trainee_administration/TraineeAdministrationApplication.java
@@ -0,0 +1,12 @@
+package com.btc.lecoorde.trainee_administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineeAdministrationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/btc/lecoorde/trainee_administration/TraineeAdministrationApplicationTests.java
@@ -0,0 +1,18 @@
+package com.btc.lecoorde.trainee_administration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TraineeAdministrationApplication.class)
+@WebAppConfiguration
+public class TraineeAdministrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
