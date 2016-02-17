a891119a445ad048263f1199e6097ed1518f2be9
LECOORDE <lennard.coordes@btc-ag.com>
2016-02-17 16:03:39 +0100

    Merge remote-tracking branch 'origin/master' aufruf mit factory eingebaut

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -9,9 +9,12 @@ angular.module('tutorialApp', ['ngRoute'])
     .factory('TraineeService', ['$http', '$q', function ($http, $q) {
         return {
             getTrainee: function (id) {
-               return $http.get('http://localhost:7070/oneuser/' + id).then(function (response) {
+               return $http.get('http://localhost:7070/trainee/' + id).then(function (response) {
                     return response.data;
-                });
+                },function (errResponse) {
+                   console.error('Error while fetching users');
+                   return $q.reject(errResponse);
+               });
 
             },
             getTrainees: function () {
@@ -33,16 +36,27 @@ angular.module('tutorialApp', ['ngRoute'])
     .controller('OneTraineeCtrl', ['$scope', 'TraineeService', function ($scope, TraineeService) {
         var self = this;
         self.trainees = [];
+        self.trainee={id:null,forename:'',lastname:'',job:'',birthday:1,start_of_training:1};
         self.fetchAllTrainees = function () {
             TraineeService.getTrainees().then(
                 function (d) {
                     self.trainees = d;
                 },
-
                 function (errResponse) {
                     console.error('Error while fetching Currencies');
                 }
             );
         };
+        self.getSingleTrainee=function(id){
+            TraineeService.getTrainee(id).then(function(d){
+                self.trainee=d;
+                return self.trainee;
+            },
+
+                function (errResponse) {
+                    console.error('Error while fetching Currencies');
+                }
+            )
+        }
         self.fetchAllTrainees();
     }]);
