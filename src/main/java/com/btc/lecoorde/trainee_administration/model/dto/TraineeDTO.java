cbce5c46c9e465e0bf24917bfb795d898918f8a2
DESIMON <denis.simon@btc-ag.com>
2016-02-25 14:27:16 +0100

    Merge branches 'delete_update_feature' and 'master' of https://github.com/lecoorde/trainee_administration into delete_update_feature
    # Conflicts:
    #	src/main/java/com/btc/lecoorde/trainee_administration/controller/TraineeController.java

--- src/main/java/com/btc/lecoorde/trainee_administration/model/dto/TraineeDTO.java
+++ src/main/java/com/btc/lecoorde/trainee_administration/model/dto/TraineeDTO.java
@@ -1,6 +1,8 @@
 package com.btc.lecoorde.trainee_administration.model.dto;
 
-import java.util.Date;
+import sun.awt.image.ImageWatched;
+
+import java.util.*;
 
 /**
  * Created by Denis Simon on 17.02.2016.
@@ -19,20 +21,35 @@ public class TraineeDto {
 
     private Date start_of_training;
 
+    private int jobOrdinal;
+
     private String departmentName;
+
+    private Long departmentId;
 
     private String locationName;
 
+    private Long locationId;
 
-    public TraineeDto(Long id, String lastName, String forename, String jobName, Date birthday, Date start_of_training, String departmentName, String locationName) {
+    private LinkedList<Long> skillIds;
+
+    private LinkedList<String> skillNames;
+
+
+    public TraineeDto(Long id, String lastName, String forename, String jobName, Date birthday, Date start_of_training, int jobOrdinal, String departmentName, Long departmentId, String locationName, Long locationId,LinkedList<String> skillNames, LinkedList<Long> skillIds) {
         this.id = id;
         this.lastName = lastName;
         this.forename = forename;
         this.jobName = jobName;
         this.birthday = birthday;
         this.start_of_training = start_of_training;
+        this.jobOrdinal = jobOrdinal;
         this.departmentName = departmentName;
+        this.departmentId = departmentId;
         this.locationName = locationName;
+        this.locationId = locationId;
+        this.skillIds = skillIds;
+        this.skillNames = skillNames;
     }
 
     public TraineeDto() {
@@ -48,6 +65,46 @@ public class TraineeDto {
                 ", birthday=" + birthday +
                 ", start_of_training=" + start_of_training +
                 '}';
+    }
+
+    public int getJobOrdinal() {
+        return jobOrdinal;
+    }
+
+    public void setJobOrdinal(int jobOrdinal) {
+        this.jobOrdinal = jobOrdinal;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Long locationId) {
+        this.locationId = locationId;
+    }
+
+    public LinkedList<Long> getSkillIds() {
+        return skillIds;
+    }
+
+    public void setSkillIds(LinkedList<Long> skillIds) {
+        this.skillIds = skillIds;
+    }
+
+    public LinkedList<String> getSkillNames() {
+        return skillNames;
+    }
+
+    public void setSkillNames(LinkedList<String> skillNames) {
+        this.skillNames = skillNames;
     }
 
     public String getDepartmentName() {
